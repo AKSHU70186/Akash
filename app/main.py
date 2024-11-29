e3920a6562905393bd77d6ba915471c795cf860c
Your Name <akthakur8080@gmail.com>
2024-11-29 11:52:40 +0530

    Create proper project structure

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ async def scrape(url: str = Form(...)):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
